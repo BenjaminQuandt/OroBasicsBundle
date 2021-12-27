43dc56544876470bba990257a322279f8fbf3409
Benjamin Quandt <benjamin.quandt@dmk-ebusiness.com>
2021-12-27 13:48:48 +0100

    fix some bundle errors

--- src/Form/Type/OroUsernameChoiceType.php
+++ src/Form/Type/OroUsernameChoiceType.php
@@ -7,54 +7,21 @@ namespace EHDev\BasicsBundle\Form\Type;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 use Oro\Bundle\EntityConfigBundle\Form\Type\ChoiceType;
 use Oro\Bundle\UserBundle\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class OroUsernameChoiceType extends AbstractType
 {
-    /**
-     * @var DoctrineHelper
-     */
-    private $helper;
-
-    /**
-     * OroUsernameChoiceType constructor.
-     */
-    public function __construct(DoctrineHelper $helper)
-    {
-        $this->helper = $helper;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefault('choices', $this->getUsername());
+        $resolver->setDefault('class', User::class);
+        $resolver->setDefault('choice_label', 'username');
+        $resolver->setDefault('choice_value', 'username');
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getParent()
     {
-        return ChoiceType::class;
-    }
-
-    /**
-     * Fetches a list of usernames with ids.
-     */
-    private function getUsername(): iterable
-    {
-        $qb = $this->helper
-            ->getEntityRepository(User::class)
-            ->createQueryBuilder('u');
-
-        $qb->select('u.username');
-        $qb->orderBy('u.username');
-
-        foreach ($qb->getQuery()->getArrayResult() as $row) {
-            yield $row['username'] => $row['username'];
-        }
+        return EntityType::class;
     }
 }
